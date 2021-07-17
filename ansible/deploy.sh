d99f1e6319654704c2559473392f01a0cb075573
Ainara Garzo <ainara.garzo148@comunidadunir.net>
2021-07-17 23:56:04 +0200

    solving ssh connection problems

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -2,6 +2,7 @@
 
 #ansible-playbook -i hosts clone-repo.yaml
 ansible-playbook -i hosts deploy-terraform.yaml
+sudo apt-get install sshpass
 ansible-playbook -i hosts deploy-common.yaml
 ansible-playbook -i hosts deploy-docker.yaml
 ansible-playbook -i master deploy-k8s.yaml
