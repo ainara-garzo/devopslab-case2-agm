2531aea00419a9e7886293b7c2070b0cd8311db8
Ainara Garzo <ainara.garzo148@comunidadunir.net>
2021-07-17 23:19:45 +0200

    changed deploy

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -3,5 +3,5 @@
 #ansible-playbook -i hosts clone-repo.yaml
 ansible-playbook -i hosts deploy-terraform.yaml
 ansible-playbook -i hosts deploy-common.yaml
-ansible-playbook -i hosts -l vm-worker01 deploy-docker.yaml
-ansible-playbook -i hosts -l vm-master deploy-k8s.yaml
+ansible-playbook -i hosts deploy-docker.yaml
+ansible-playbook -i master deploy-k8s.yaml
