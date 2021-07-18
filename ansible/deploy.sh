cb302c52eed554ba33cc228327c2619d0897e229
Ainara Garzo <ainara.garzo148@comunidadunir.net>
2021-07-18 12:45:13 +0200

    deploy.sh changed

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-ansible-playbook -i hosts deploy-terraform.yaml
-ansible-playbook -i hosts create-user.yaml
-ansible-playbook -i hosts deploy-common.yaml
-ansible-playbook -i hosts deploy-docker.yaml
+#ansible-playbook -i hosts deploy-terraform.yaml
+#ansible-playbook -i hosts create-user.yaml
+#ansible-inventory -vvvvvv -i hosts_info.yaml --list
+ansible-playbook -i hosts -l nfs-node clone-repo.yaml
+ansible-playbook -i hosts -l nfs-node deploy-common.yaml
+ansible-playbook -i hosts -l nfs-node deploy-docker.yaml
 ansible-playbook -i master deploy-k8s.yaml
