5bb00dfe5df5a5be03678c3adda2082e9454d9d1
Ainara Garzo <ainara.garzo148@comunidadunir.net>
2021-07-18 12:53:05 +0200

    hosts application change

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -3,7 +3,7 @@
 #ansible-playbook -i hosts deploy-terraform.yaml
 #ansible-playbook -i hosts create-user.yaml
 #ansible-inventory -vvvvvv -i hosts_info.yaml --list
-ansible-playbook -i hosts -l nfs-node clone-repo.yaml
-ansible-playbook -i hosts -l nfs-node deploy-common.yaml
-ansible-playbook -i hosts -l nfs-node deploy-docker.yaml
+ansible-playbook -i hosts clone-repo.yaml
+ansible-playbook -i hosts deploy-common.yaml
+ansible-playbook -i hosts deploy-docker.yaml
 ansible-playbook -i master deploy-k8s.yaml
