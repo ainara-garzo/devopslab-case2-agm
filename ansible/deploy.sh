993068b37241276bd59dbbf7445535954610df16
Ainara Garzo <ainara.garzo148@comunidadunir.net>
2021-07-18 10:35:52 +0200

    create ansible user

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-#ansible-playbook -i hosts clone-repo.yaml
 ansible-playbook -i hosts deploy-terraform.yaml
-sudo apt-get install sshpass
+ansible-playbook -i hosts create-user.yaml
 ansible-playbook -i hosts deploy-common.yaml
 ansible-playbook -i hosts deploy-docker.yaml
 ansible-playbook -i master deploy-k8s.yaml
